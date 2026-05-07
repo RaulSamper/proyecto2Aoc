365c8fb1f975baa8e07e53d88eeab70338c916e6
JaimeSanchez8MILFOR <899744@unizar.es>
2026-05-07 22:45:01 +0200

    tests y demas

--- proyecto2/Moodle2026_p2/mov.sh
+++ proyecto2/Moodle2026_p2/mov.sh
@@ -2,6 +2,6 @@ mkdir -p WORK
 ghdl -i --ieee=synopsys -fexplicit --workdir=WORK *.vhd
 ghdl --gen-makefile -fexplicit --ieee=synopsys --workdir=WORK testbench > Makefile
 ghdl -m --ieee=synopsys -fexplicit --workdir=WORK testbench
-ghdl -r --ieee=synopsys -fexplicit --workdir=WORK testbench --stop-time=500us --wave=test.ghw
+ghdl -r --ieee=synopsys -fexplicit --workdir=WORK testbench --stop-time=5us --wave=test.ghw
 gtkwave test.ghw &
 ghdl --clean --workdir=WORK
